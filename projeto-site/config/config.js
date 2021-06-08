a8a5f020bdb546fcd152d7dd02c12e3d48d1c9a3
winicius-silva <winicius.silva@bandtec.com.br>
2021-06-08 16:57:08 -0300

    primeiro commit

---
+++ projeto-site/config/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+
+  development: {
+    dialect: "sqlite",
+    storage: "./db.development.sqlite"
+  },
+  test: {
+    dialect: "sqlite",
+    storage: ":memory:"
+  },
+  production: {
+    username: 'omnitrx',
+    password: '#Gfgrupo6',
+    database: 'omnitrx',
+    host: 'omnitrx.database.windows.net',
+    dialect: 'mssql',
+    xuse_env_variable: 'DATABASE_URL',
+    dialectOptions: {
+      options: {
+        encrypt: true
+      }
+    },
+    pool: { 
+      max: 5,
+      min: 1,
+      acquire: 5000,
+      idle: 30000,
+      connectTimeout: 5000
+    }
+  }
+};
+ 
